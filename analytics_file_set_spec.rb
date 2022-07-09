87cfe712cdaadb0d68ee16f882e7f2f04aaa1ae8
mahaylab2004 <108966246+mahaylab2004@users.noreply.github.com>
2022-07-09 00:08:07 -0500

    Create analytics_file_set_spec.rb

---
+++ analytics_file_set_spec.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: analytics_file_sets
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe AnalyticsFileSet do
+  # it { is_expected.to have_many :analytics_files }
+end
