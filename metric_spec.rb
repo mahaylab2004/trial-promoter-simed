f51b41706bc5e882a7cead488368466d8b78a003
mahaylab2004 <108966246+mahaylab2004@users.noreply.github.com>
2022-07-09 00:14:33 -0500

    Create metric_spec.rb

---
+++ metric_spec.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: metrics
+#
+#  id         :integer          not null, primary key
+#  source     :string
+#  data       :text
+#  message_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe Metric, type: :model do
+  it { is_expected.to validate_presence_of :data }
+  it { is_expected.to validate_presence_of :source }
+  it { is_expected.to enumerize(:source).in(:buffer, :twitter, :facebook, :instagram, :google_analytics) }
+  it { is_expected.to belong_to(:message) }
+  it { is_expected.to belong_to(:post) }
+  it { is_expected.to serialize(:data).as(Hash) }
+end
