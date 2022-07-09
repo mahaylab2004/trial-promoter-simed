7984c62c370b83f4af38a70d773860157be5f23d
mahaylab2004 <108966246+mahaylab2004@users.noreply.github.com>
2022-07-09 00:21:50 -0500

    Create social_media_profile_policy_spec.rb

---
+++ social_media_profile_policy_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe SocialMediaProfilePolicy, type: :policy do
+  subject { SocialMediaProfilePolicy.new(user, social_media_profile) }
+
+  let(:social_media_profile) { create(:social_media_profile) }
+
+  context "for a user with no role" do
+    let(:user) { create(:user) }
+
+    it { should_not be_permitted_to(:sync_with_buffer) }
+    it { should_not be_permitted_to(:index) }
+    it { should_not be_permitted_to(:edit) }
+    it { should_not be_permitted_to(:update) }
+  end
+
+  context "for an administrator" do
+    let(:user) { create(:administrator) }
+
+    it { should be_permitted_to(:sync_with_buffer) }
+    it { should be_permitted_to(:index) }
+    it { should be_permitted_to(:edit) }
+    it { should be_permitted_to(:update) }
+  end
+end
