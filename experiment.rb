40e6fe419ad5c4db7502ef6734e5bba50d692b42
mahaylab2004 <108966246+mahaylab2004@users.noreply.github.com>
2022-07-08 23:32:43 -0500

    Create experiment.rb

---
+++ experiment.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :experiment do
+    name 'Name'
+    message_distribution_start_date DateTime.new(2017, 1, 1, 12, 0, 0)
+  end
+
+  factory :invalid_experiment, parent: :experiment do
+    name nil
+  end
+end
