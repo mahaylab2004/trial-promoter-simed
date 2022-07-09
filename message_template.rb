a1b8fe84be037a158663fc8694385f32bdc331fc
mahaylab2004 <108966246+mahaylab2004@users.noreply.github.com>
2022-07-08 23:34:02 -0500

    Create message_template.rb

---
+++ message_template.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :message_template do
+    content "Some content for Twitter with a {parameter}"
+    platforms [:twitter]
+    promoted_website_url 'http://example.com'
+  end
+  
+  factory :invalid_message_template, parent: :message_template do
+    content nil
+  end
+end
