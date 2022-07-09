9c7170d4be52cc82123f77f29cb555bc9e8aa211
mahaylab2004 <108966246+mahaylab2004@users.noreply.github.com>
2022-07-08 23:56:33 -0500

    Create bijective_function_spec.rb

---
+++ bijective_function_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'yaml'
+
+RSpec.describe BijectiveFunction do
+  it 'only uses a alphabet consisting of lowercase letters and numbers' do
+    # Click Meter does not distinguish between names that differe in case.
+    # Example: 9nl.es/name and 9nl.es/NAME are the same!
+    # So we restrict the alphabet for the Bijective function to lowercase only
+    expect(BijectiveFunction::ALPHABET).to eq("bcdfghjklmnpqrstvwxyz0123456789".split(//))
+  end
+  
+  it 'encodes an integer' do
+    expect(BijectiveFunction.encode(1250000000)).to eq('cqzt9qz')
+  end
+  
+  it 'decodes a string' do
+    expect(BijectiveFunction.decode('cqzt9qz')).to eq(1250000000)
+  end
+end
