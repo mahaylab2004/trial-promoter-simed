013931488168a4cab6063bcbc431e53cd0bc0b98
mahaylab2004 <108966246+mahaylab2004@users.noreply.github.com>
2022-07-08 23:33:44 -0500

    Create message_generation_parameter_set.rb

---
+++ message_generation_parameter_set.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :message_generation_parameter_set do
+    association :message_generating, factory: :experiment
+    social_network_choices ['facebook']
+    medium_choices ['ad']
+    image_present_choices :no_messages
+    number_of_cycles 5
+    number_of_messages_per_social_network 1
+    number_of_days_between_posting 1
+  end
+
+  factory :invalid_message_generation_parameter_set, parent: :message_generation_parameter_set do
+    number_of_cycles nil
+    number_of_messages_per_social_network nil
+  end
+end
