5f97bf41b0b56250cd73e12f83cbe20861b5e555
mahaylab2004 <108966246+mahaylab2004@users.noreply.github.com>
2022-07-08 23:33:03 -0500

    Create image.rb

---
+++ image.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :image do
+    sequence :url do |n|
+      "http://www.image-#{n}.com"
+    end
+    sequence :original_filename do |n|
+      "http://www.filename-#{n}.com"
+    end
+  end
+end
