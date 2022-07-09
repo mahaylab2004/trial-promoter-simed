2ac92edf2a7666979198a1f911a16cf38f2a312d
mahaylab2004 <108966246+mahaylab2004@users.noreply.github.com>
2022-07-08 23:36:37 -0500

    Create user.rb

---
+++ user.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password 'password'
+    password_confirmation 'password'
+    role 'user'
+  end
+
+  factory :administrator, parent: :user do
+    role 'administrator'
+  end
+end
